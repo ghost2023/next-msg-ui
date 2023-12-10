0493c3160f722ab415189e4362f9b081192a9423
ghost2023 <nathnaelwonisha@gmail.com>
2023-12-10 14:58:59 +0300

    cleaned daisy ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -38,6 +37,11 @@ const config: Config = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
+  },
+
+  daisyui: {
+    themes: false,
+    base: false,
   },
 
   plugins: [require("tailwindcss-animate"), require("daisyui")],
