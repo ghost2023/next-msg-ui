9f87949ddd8b7382247ecb717736f125f81302f0
ghost2023 <nathnaelwonisha@gmail.com>
2023-12-12 16:10:11 +0300

    added function to convert formdata to obj

--- utils.ts
+++ utils.ts
@@ -1,6 +1,24 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const FormDataToObject = (formData: FormData) => {
+  const obj: Record<string, any> = {};
+  for (const [key, value] of Array.from(formData.entries())) {
+    if (obj[key] === undefined) {
+      // If the key doesn't exist in the object, set it to the value
+      obj[key] = value;
+    } else {
+      // If the key already exists, convert it to an array if needed
+      if (!Array.isArray(obj[key])) {
+        obj[key] = [obj[key]];
+      }
+      obj[key].push(value);
+    }
+  }
+
+  return obj;
+};
